3c771e546978f1dae3a5a8531052c4f4749791d4
Christian Todd <ChristianTodd27@icloud.com>
2016-10-04 08:53:26 -0400

    Complete Problem 1 in Ch5.

--- ch5/ch5-ex.js
+++ ch5/ch5-ex.js
@@ -1,1 +1,7 @@
 //PROBLEM 1 //////////////////////////////////////////////////////////////////////////////
+function flattenArray(inputArr) {
+    return inputArr.reduce(function (prev, curr) { return prev.concat(curr); });
+}
+;
+console.log(flattenArray([[1, 2, 3], [4, 5], [6]]));
+// → [1, 2, 3, 4, 5, 6]
--- ch5/ch5-ex.ts
+++ ch5/ch5-ex.ts
@@ -1,1 +1,8 @@
 //PROBLEM 1 //////////////////////////////////////////////////////////////////////////////
+
+function flattenArray(inputArr: any[]): any[] {
+    return inputArr.reduce((prev, curr) => prev.concat(curr));
+};
+
+console.log(flattenArray([[1, 2, 3], [4, 5], [6]]));
+// → [1, 2, 3, 4, 5, 6]
