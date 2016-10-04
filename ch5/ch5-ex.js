a90ec7678a39fa0f03e1d48d2e98919bc2968d6d
Christian Todd <ChristianTodd27@icloud.com>
2016-10-04 11:14:46 -0400

    Start on Problem 2 of Ch5. Create ancestry.ts to hold data for Problem 2.

--- ch5/ch5-ex.js
+++ ch5/ch5-ex.js
@@ -1,7 +1,22 @@
 //PROBLEM 1 //////////////////////////////////////////////////////////////////////////////
+"use strict";
 function flattenArray(inputArr) {
     return inputArr.reduce(function (prev, curr) { return prev.concat(curr); });
 }
 ;
 console.log(flattenArray([[1, 2, 3], [4, 5], [6]]));
 // → [1, 2, 3, 4, 5, 6]
+//PROBLEM 2 /////////////////////////////////////////////////////////////////////////////
+var ancestry_1 = require('./ancestry');
+var ancestry = JSON.parse(ancestry_1.ANCESTRY_FILE);
+function average(array) {
+    function plus(a, b) { return a + b; }
+    return array.reduce(plus) / array.length;
+}
+var byName = {};
+ancestry.forEach(function (person) {
+    byName[person.name] = person;
+});
+function motherChildAgeDifference(input) {
+}
+// → 31.2
--- ch5/ch5-ex.ts
+++ ch5/ch5-ex.ts
@@ -6,3 +6,27 @@ function flattenArray(inputArr: any[]): any[] {
 
 console.log(flattenArray([[1, 2, 3], [4, 5], [6]]));
 // → [1, 2, 3, 4, 5, 6]
+
+//PROBLEM 2 /////////////////////////////////////////////////////////////////////////////
+
+import {ANCESTRY_FILE} from './ancestry';
+let ancestry = JSON.parse(ANCESTRY_FILE);
+
+function average(array: number[]): number {
+  function plus(a: number, b: number) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+let byName = {};
+ancestry.forEach(function(person:any) {
+  byName[person.name] = person;
+});
+
+
+function motherChildAgeDifference(input: any[]){
+
+}
+
+
+
+// → 31.2
