8bfb8cbe394a3ae36d86e04603fb607c642a0d6e
Christian Todd <ChristianTodd27@icloud.com>
2016-11-02 19:17:36 -0400

    Chapter 6 initalized with exercise instructions. .ts and .js files also initalized.

---
+++ ch6/ch6-ex.ts
@@ -0,0 +1,30 @@
+//PROBLEM 1 /////////////////////////////////////////////////////////////////////////////
+
+console.log(new Vector(1,2).plus(new Vector(2,3))); 
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1,2).minus(new Vector(2,3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3,4).length);
+// → 5
+
+//PROBLEM 2 /////////////////////////////////////////////////////////////////////////////
+
+var sc = new StretchCell(new TextCell("abc"), 1, 2);
+console.log(sc.minWidth());
+// → 3
+console.log(sc.minHeight());
+// → 2
+console.log(sc.draw(3, 2));
+// → ["abc", "   "]
+
+//PROBLEM 3 /////////////////////////////////////////////////////////////////////////////
+
+logFive(new ArraySeq([1, 2]));
+// → 1
+// → 2
+logFive(new RangeSeq(100, 1000));
+// → 100
+// → 101
+// → 102
+// → 103
+// → 104
