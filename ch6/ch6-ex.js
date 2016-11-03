b834679342876fe62e269482868179ce2a2186fe
Christian Todd <ChristianTodd27@icloud.com>
2016-11-02 22:21:06 -0400

    Problem 1 in Ch6 complete.

--- ch6/ch6-ex.js
+++ ch6/ch6-ex.js
@@ -1,4 +1,17 @@
 //PROBLEM 1 /////////////////////////////////////////////////////////////////////////////
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+Vector.prototype.plus = function (alt) {
+    return new Vector(this.x + alt.x, this.y + alt.y);
+};
+Vector.prototype.minus = function (alt) {
+    return new Vector(this.x - alt.x, this.y - alt.y);
+};
+Object.defineProperty(Vector.prototype, 'length', {
+    get: function () { return Math.sqrt((this.x * this.x) + (this.y * this.y)); }
+});
 console.log(new Vector(1, 2).plus(new Vector(2, 3)));
 // → Vector{x: 3, y: 5}
 console.log(new Vector(1, 2).minus(new Vector(2, 3)));
@@ -6,6 +19,7 @@ console.log(new Vector(1, 2).minus(new Vector(2, 3)));
 console.log(new Vector(3, 4).length);
 // → 5
 //PROBLEM 2 /////////////////////////////////////////////////////////////////////////////
+/*
 var sc = new StretchCell(new TextCell("abc"), 1, 2);
 console.log(sc.minWidth());
 // → 3
@@ -13,7 +27,9 @@ console.log(sc.minHeight());
 // → 2
 console.log(sc.draw(3, 2));
 // → ["abc", "   "]
+
 //PROBLEM 3 /////////////////////////////////////////////////////////////////////////////
+
 logFive(new ArraySeq([1, 2]));
 // → 1
 // → 2
@@ -22,4 +38,5 @@ logFive(new RangeSeq(100, 1000));
 // → 101
 // → 102
 // → 103
-// → 104 
+// → 104
+*/ 
--- ch6/ch6-ex.ts
+++ ch6/ch6-ex.ts
@@ -1,4 +1,20 @@
 //PROBLEM 1 /////////////////////////////////////////////////////////////////////////////
+function Vector (x: number,y: number){
+    this.x = x;
+    this.y = y;
+}
+
+Vector.prototype.plus = function (alt: any){
+    return new Vector(this.x + alt.x, this.y + alt.y);
+}
+
+Vector.prototype.minus = function(alt: any){
+    return new Vector(this.x - alt.x, this.y - alt.y);
+}
+
+Object.defineProperty(Vector.prototype, 'length', {
+    get: function(){return Math.sqrt((this.x * this.x) + (this.y * this.y))}
+});
 
 console.log(new Vector(1,2).plus(new Vector(2,3))); 
 // → Vector{x: 3, y: 5}
@@ -8,7 +24,7 @@ console.log(new Vector(3,4).length);
 // → 5
 
 //PROBLEM 2 /////////////////////////////////////////////////////////////////////////////
-
+/*
 var sc = new StretchCell(new TextCell("abc"), 1, 2);
 console.log(sc.minWidth());
 // → 3
@@ -28,3 +44,4 @@ logFive(new RangeSeq(100, 1000));
 // → 102
 // → 103
 // → 104
+*/
