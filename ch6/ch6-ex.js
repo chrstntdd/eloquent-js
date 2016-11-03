658f731b4ea997d58a5b1f2581194263724a8a61
Christian Todd <ChristianTodd27@icloud.com>
2016-11-02 23:16:22 -0400

    Begin work on Ch6 Problem 2.

--- ch6/ch6-ex.js
+++ ch6/ch6-ex.js
@@ -1,25 +1,39 @@
 //PROBLEM 1 /////////////////////////////////////////////////////////////////////////////
-function Vector(x, y) {
+/*
+function Vector (x: number,y: number){
     this.x = x;
     this.y = y;
 }
-Vector.prototype.plus = function (alt) {
+
+Vector.prototype.plus = function (alt: any){
     return new Vector(this.x + alt.x, this.y + alt.y);
-};
-Vector.prototype.minus = function (alt) {
+}
+
+Vector.prototype.minus = function(alt: any){
     return new Vector(this.x - alt.x, this.y - alt.y);
-};
+}
+
 Object.defineProperty(Vector.prototype, 'length', {
-    get: function () { return Math.sqrt((this.x * this.x) + (this.y * this.y)); }
+    get: function(){return Math.sqrt((this.x * this.x) + (this.y * this.y))}
 });
-console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+
+console.log(new Vector(1,2).plus(new Vector(2,3)));
 // → Vector{x: 3, y: 5}
-console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+console.log(new Vector(1,2).minus(new Vector(2,3)));
 // → Vector{x: -1, y: -1}
-console.log(new Vector(3, 4).length);
+console.log(new Vector(3,4).length);
 // → 5
+*/
 //PROBLEM 2 /////////////////////////////////////////////////////////////////////////////
-/*
+//from textbook
+function TextCell(text) {
+    this.text = text.split("\n");
+}
+function StretchCell(inner, width, height) {
+    this.inner = inner;
+    this.width = width;
+    this.height = height;
+}
 var sc = new StretchCell(new TextCell("abc"), 1, 2);
 console.log(sc.minWidth());
 // → 3
@@ -27,9 +41,8 @@ console.log(sc.minHeight());
 // → 2
 console.log(sc.draw(3, 2));
 // → ["abc", "   "]
-
 //PROBLEM 3 /////////////////////////////////////////////////////////////////////////////
-
+/*
 logFive(new ArraySeq([1, 2]));
 // → 1
 // → 2
--- ch6/ch6-ex.ts
+++ ch6/ch6-ex.ts
@@ -1,4 +1,5 @@
 //PROBLEM 1 /////////////////////////////////////////////////////////////////////////////
+/*
 function Vector (x: number,y: number){
     this.x = x;
     this.y = y;
@@ -22,9 +23,20 @@ console.log(new Vector(1,2).minus(new Vector(2,3)));
 // → Vector{x: -1, y: -1}
 console.log(new Vector(3,4).length);
 // → 5
-
+*/
 //PROBLEM 2 /////////////////////////////////////////////////////////////////////////////
-/*
+
+//from textbook
+function TextCell(text: any) {
+    this.text = text.split("\n");
+  }
+
+function StretchCell (inner: any, width: number, height: number){
+    this.inner = inner;
+    this.width = width;
+    this.height = height;
+}
+
 var sc = new StretchCell(new TextCell("abc"), 1, 2);
 console.log(sc.minWidth());
 // → 3
@@ -34,7 +46,7 @@ console.log(sc.draw(3, 2));
 // → ["abc", "   "]
 
 //PROBLEM 3 /////////////////////////////////////////////////////////////////////////////
-
+/*
 logFive(new ArraySeq([1, 2]));
 // → 1
 // → 2
