70c7e79a134a693b2add9a266755ce9b46b20493
Christian Todd <ChristianTodd27@icloud.com>
2016-11-07 14:36:14 -0500

    Create World class with toString method.

--- ch7/ch7-proj.js
+++ ch7/ch7-proj.js
@@ -80,26 +80,30 @@ var BouncingCritter = (function () {
     };
     return BouncingCritter;
 }());
-function World(map, legend) {
-    var grid = new Grid(map[0].length, map.length);
-    this.grid = grid;
-    this.legend = legend;
-    map.forEach(function (line, y) {
-        for (var x = 0; x < line.length; x++)
-            grid.set(new ch6_ex_1.Vector(x, y), elementFromChar(legend, line[x]));
-    });
-}
-World.prototype.toString = function () {
-    var output = "";
-    for (var y = 0; y < this.grid.height; y++) {
-        for (var x = 0; x < this.grid.width; x++) {
-            var element = this.grid.get(new ch6_ex_1.Vector(x, y));
-            output += charFromElement(element);
-        }
-        output += "\n";
+var World = (function () {
+    function World(map, legend) {
+        var grid = new Grid(map[0].length, map.length);
+        this.grid = grid;
+        this.legend = legend;
+        map = map.forEach(function (line, y) {
+            for (var x = 0; x < line.length; x++)
+                grid.set(new ch6_ex_1.Vector(x, y), elementFromChar(legend, line[x]));
+        });
     }
-    return output;
-};
+    World.prototype.toString = function () {
+        var output = '';
+        for (var y = 0; y < this.grid.height; y++) {
+            for (var x = 0; x < this.grid.width; x++) {
+                var element = this.grid.get(new ch6_ex_1.Vector(x, y));
+                output += charFromElement(element);
+            }
+            output += '\n';
+        }
+        return output;
+    };
+    ;
+    return World;
+}());
 var world = new World(plan, { "#": Wall,
     "o": BouncingCritter });
 console.log(world.toString());
--- ch7/ch7-proj.ts
+++ ch7/ch7-proj.ts
@@ -89,6 +89,35 @@ class BouncingCritter{
     }
 }
 
+class World{
+    grid;
+    legend;
+    map;
+
+
+    constructor(map, legend) {
+        var grid = new Grid(map[0].length, map.length);
+        this.grid = grid;
+        this.legend = legend;
+        map = map.forEach(function (line, y) {
+            for (var x = 0; x < line.length; x++)
+                grid.set(new Vector(x, y), elementFromChar(legend, line[x]));
+        })
+    }
+
+    toString(){
+        let output = '';
+        for(var y = 0; y < this.grid.height; y++){
+            for (var x = 0; x < this.grid.width; x++){
+                let element = this.grid.get(new Vector(x,y));
+                output += charFromElement(element);
+            }
+            output+= '\n';
+        }
+        return output;
+    };
+}
+
 var world = new World(plan, {"#": Wall,
                              "o": BouncingCritter});
 console.log(world.toString());
