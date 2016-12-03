e0c1c19ac9c14769848adceee53a48cf5d4d0722
Christian Todd <ChristianTodd27@icloud.com>
2016-12-02 19:07:32 -0500

    chapter 8 complete!

--- src/ch8/ch8-ex.js
+++ src/ch8/ch8-ex.js
@@ -19,4 +19,50 @@ function reliableMultiply(a, b) {
     }
 }
 console.log(reliableMultiply(8, 8));
-// → 64 
+// → 64
+//PROBLEM 2
+//Provided by text
+var box = {
+    locked: true,
+    unlock: function () {
+        this.locked = false;
+    },
+    lock: function () {
+        this.locked = true;
+    },
+    _content: [],
+    get content() {
+        if (this.locked)
+            throw new Error("Locked!");
+        return this._content;
+    }
+};
+function withBoxUnlocked(body) {
+    var locked = box.locked;
+    if (!locked) {
+        return body();
+    }
+    box.unlock();
+    try {
+        body();
+    }
+    catch (error) {
+        console.log('There was an error: ' + error);
+    }
+    finally {
+        box.lock();
+    }
+}
+withBoxUnlocked(function () {
+    box.content.push("gold piece");
+});
+try {
+    withBoxUnlocked(function () {
+        throw new Error("Pirates on the horizon! Abort!");
+    });
+}
+catch (e) {
+    console.log("Error raised:", e);
+}
+console.log(box.locked);
+// → true 
--- src/ch8/ch8-ex.ts
+++ src/ch8/ch8-ex.ts
@@ -23,3 +23,51 @@ function reliableMultiply(a: number, b: number): number {
 
 console.log(reliableMultiply(8, 8));
 // → 64
+
+
+//PROBLEM 2
+
+//Provided by text
+var box = {
+  locked: true,
+  unlock: function () {
+    this.locked = false;
+  },
+  lock: function () {
+    this.locked = true;
+  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  }
+};
+
+function withBoxUnlocked(body) {
+  let locked = box.locked;
+  if (!locked) {
+    return body();
+  }
+  box.unlock();
+  try {
+    body();
+  } catch (error) {
+    console.log(error);
+  } finally {
+    box.lock();
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised:", e);
+}
+console.log(box.locked);
+// → true
