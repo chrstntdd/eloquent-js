e6e99bbec0ef712ca1d6a7705f65aaa0bb8f80a1
Christian Todd <ChristianTodd27@icloud.com>
2016-12-09 18:30:12 -0500

    Finish chapter 9 and add regex explaination for problem 3.

--- src/ch9/ch9-ex.js
+++ src/ch9/ch9-ex.js
@@ -33,11 +33,11 @@ verify(/\b[a-df-z]+\b/,
 function verify(regexp, yes, no) {
   // Ignore unfinished exercises
   if (regexp.source == "...") return;
-  yes.forEach(function(s) {
+  yes.forEach(function (s) {
     if (!regexp.test(s))
       console.log("Failure to match '" + s + "'");
   });
-  no.forEach(function(s) {
+  no.forEach(function (s) {
     if (regexp.test(s))
       console.log("Unexpected match for '" + s + "'");
   });
@@ -49,3 +49,22 @@ var text = "'I'm the cook,' he said, 'it's my job.'";
 // Change this call.
 console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2'));
 // → "I'm the cook," he said, "it's my job."
+
+
+//PROBLEM 3
+// Fill in this regular expression.
+var number = /^(\+|-|)(\d+(\.\d*)?|\.\d+)([eE](\+|-|)\d+)?$/i;
+
+// Tests:
+["1", "-1", "+15", "1.55", ".5", "5.", "1.3e2", "1E-4",
+  "1e+12"
+].forEach(function (s) {
+  if (!number.test(s))
+    console.log("Failed to match '" + s + "'");
+});
+["1a", "+-1", "1.2.3", "1+1", "1e4.5", ".5.", "1f5",
+  "."
+].forEach(function (s) {
+  if (number.test(s))
+    console.log("Incorrectly accepted '" + s + "'");
+});
