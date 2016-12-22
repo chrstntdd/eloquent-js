b05f73d72b8a69d199438428f912efdff72be03d
Christian Todd <ChristianTodd27@icloud.com>
2016-12-22 16:14:19 -0500

    continue with chapter project. add specialForms and evaluate.

--- src/ch11/ch11-proj.js
+++ src/ch11/ch11-proj.js
@@ -67,3 +67,29 @@ function parse(program) {
 }
 
 console.log(parse("+(a,10)"));
+
+function evaluate(expr, env){
+    switch(expr.type){
+        case "value":
+        return expr.value;
+
+        case "word":
+        if (expr.name in env){
+            return env[expr.name];
+        } else {
+            throw new ReferenceError ("Undefined variable: " + expr.name);
+        }
+
+        case "apply":
+        if (expr.operator.type === "word" && expr.operator.name in specialForms){
+            return specialForms[expr.operator.name](expr.args, env);
+        }
+        let op = evaluate(expr.operator, env);
+        if (typeof op !== "function"){
+            throw new TypeError("Applying a non-function.");
+        }
+        return op.apply(null, expr.args.map((arg) => evaluate(arg,env)));
+    }
+}
+
+let specialForms = Object.create(null);
