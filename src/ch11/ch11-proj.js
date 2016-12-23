06c32635b837a317e418be21e2a25ab77e5b45b8
Christian Todd <ChristianTodd27@icloud.com>
2016-12-22 19:14:33 -0500

    complete chapter 11 problem 1. add mdn reference in solution.

--- src/ch11/ch11-proj.js
+++ src/ch11/ch11-proj.js
@@ -198,3 +198,29 @@ specialForms["fun"] = function (args, env) {
 //     "        *(base, pow(base, -(exp, 1)))))),",
 //     "   print(pow(2, 10)))");
 // → 1024
+
+// PROBLEM 1
+
+topEnv["array"] = function() {
+  return Array.prototype.slice.call(arguments);
+};
+
+topEnv["length"] = function(array) {
+  return array.length;
+};
+
+topEnv["element"] = function(array, i) {
+  return array[i];
+};
+
+run("do(define(sum, fun(array,",
+    "     do(define(i, 0),",
+    "        define(sum, 0),",
+    "        while(<(i, length(array)),",
+    "          do(define(sum, +(sum, element(array, i))),",
+    "             define(i, +(i, 1)))),",
+    "        sum))),",
+    "   print(sum(array(1, 2, 3))))");
+// → 6
+
+// REFERENCE: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
