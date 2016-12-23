188916523fbfaf8817e91e43bbf089cc3122bd53
Christian Todd <ChristianTodd27@icloud.com>
2016-12-23 18:16:41 -0500

    Complete chapter 11 problem 3.

--- src/ch11/ch11-proj.js
+++ src/ch11/ch11-proj.js
@@ -199,6 +199,8 @@ specialForms["fun"] = function (args, env) {
 //     "   print(pow(2, 10)))");
 // → 1024
 
+// BEGIN EXERCISES ///////////////////////////////////////////////////////////////
+
 // PROBLEM 1
 
 topEnv["array"] = function() {
@@ -213,17 +215,34 @@ topEnv["element"] = function(array, i) {
   return array[i];
 };
 
-run("do(define(sum, fun(array,",
-    "     do(define(i, 0),",
-    "        define(sum, 0),",
-    "        while(<(i, length(array)),",
-    "          do(define(sum, +(sum, element(array, i))),",
-    "             define(i, +(i, 1)))),",
-    "        sum))),",
-    "   print(sum(array(1, 2, 3))))");
+// run("do(define(sum, fun(array,",
+//     "     do(define(i, 0),",
+//     "        define(sum, 0),",
+//     "        while(<(i, length(array)),",
+//     "          do(define(sum, +(sum, element(array, i))),",
+//     "             define(i, +(i, 1)))),",
+//     "        sum))),",
+//     "   print(sum(array(1, 2, 3))))");
 // → 6
 
 // REFERENCE: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
 
 // PROBLEM 2
 // SEE ch11-proj.md
+
+// PROBLEM 3
+
+function skipSpace(string) {
+    let ignore = string.match(/^(\s|#.*)*/g);
+    return string.slice(ignore[0].length);
+}
+
+//For regex explaination, reference https://regex101.com 
+
+// console.log(parse("# hello\nx"));
+// // → {type: "word", name: "x"}
+
+// console.log(parse("a # one\n   # two\n()"));
+// → {type: "apply",
+//    operator: {type: "word", name: "a"},
+//    args: []}
