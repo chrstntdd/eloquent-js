0a70dacaba23e57d1caa3ac81800d2c0e8d2d89c
Christian Todd <ChristianTodd27@icloud.com>
2016-12-30 14:37:13 -0500

    complete chapter 11.

--- src/ch11/ch11-proj.js
+++ src/ch11/ch11-proj.js
@@ -246,3 +246,29 @@ function skipSpace(string) {
 // → {type: "apply",
 //    operator: {type: "word", name: "a"},
 //    args: []}
+
+// PROBLEM 4
+
+specialForms["set"] = function (args, env) {
+    if (args.length !== 2 || args[0].type !== "word") {
+        throw new SyntaxError("Bad use of set");
+    }
+    let varName = args[0].name,
+        value   = evaluate(args[1], env);
+
+    for (let scope = env; scope; scope = Object.getPrototypeOf(scope)) {
+        if (Object.prototype.hasOwnProperty.call(scope, varName)) {
+            scope[varName] = value;
+            return value;
+        }
+    }
+    throw new ReferenceError("setting undefined variable " + varName);
+};
+
+run("do(define(x, 4),",
+    "   define(setx, fun(val, set(x, val))),",
+    "   setx(50),",
+    "   print(x))");
+// → 50
+run("set(quux, true)");
+// → Some kind of ReferenceError
